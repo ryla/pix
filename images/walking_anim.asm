599ba831074a8bf200eb75f9140c877efbf31f70
Julian Ceipek <julian.ceipek@gmail.com>
2012-12-08 16:48:24 -0500

    Added walking anim change.

---
+++ images/walking_anim.asm
@@ -0,0 +1,177 @@
+:monitorFont
+    ; Font Char 0
+    ; Used...
+        ; at (6,1)
+    ;       ;
+    ;      *;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;      *;
+    ;      *;
+    ;      *;
+    DAT 0x0000, 0x00e2
+    ; Font Char 1
+    ; Used...
+        ; at (3,1)
+    ;       ;
+    ;      *;
+    ;    *  ;
+    ;      *;
+    ;       ;
+    ;      *;
+    ;      *;
+    ;    * *;
+    DAT 0x0000, 0x84ea
+    ; Font Char 2
+    ; Used...
+        ; at (2,1)
+        ; at (8,1)
+    ;*      ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    DAT 0x0100, 0x0000
+    ; Font Char 3
+    ; Used...
+        ; at (1,1)
+    ;*      ;
+    ;* * * *;
+    ;*      ;
+    ;*      ;
+    ;* *    ;
+    ;* *    ;
+    ;  *    ;
+    ;    *  ;
+    DAT 0x3f72, 0x8202
+    ; Font Char 4
+    ; Used...
+        ; at (0,0)
+        ; at (2,0)
+        ; at (3,0)
+        ; at (5,0)
+        ; at (6,0)
+        ; at (8,0)
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    DAT 0x0000, 0x0000
+    ; Font Char 5
+    ; Used...
+        ; at (0,1)
+    ;       ;
+    ;      *;
+    ;    *  ;
+    ;    *  ;
+    ;       ;
+    ;       ;
+    ;      *;
+    ;    *  ;
+    DAT 0x0000, 0x8c42
+    ; Font Char 6
+    ; Used...
+        ; at (5,1)
+    ;       ;
+    ;       ;
+    ;*      ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    DAT 0x0400, 0x0000
+    ; Font Char 7
+    ; Used...
+        ; at (4,0)
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;       ;
+    ;* *    ;
+    ;    *  ;
+    DAT 0x4040, 0x8000
+    ; Font Char 8
+    ; Used...
+        ; at (4,1)
+    ;* *    ;
+    ;*      ;
+    ;* *    ;
+    ;*   * *;
+    ;*      ;
+    ;*      ;
+    ;  *    ;
+    ;  *    ;
+    DAT 0x3fc5, 0x0808
+    ; Font Char 9
+    ; Used...
+        ; at (7,1)
+    ;*      ;
+    ;* *   *;
+    ;*   *  ;
+    ;* *    ;
+    ;*      ;
+    ;*      ;
+    ;  *    ;
+    ;  *    ;
+    DAT 0x3fca, 0x0402
+    ; Font Char 10
+    ; Used...
+        ; at (1,0)
+        ; at (7,0)
+    ;* * * *;
+    ;* * * *;
+    ;* * * *;
+    ;* * * *;
+    ;* * * *;
+    ;    * *;
+    ;* *   *;
+    ;    * *;
+    DAT 0x5f5f, 0xbfff
+:monitorPalette
+    ; Used...
+        ; at (2,1) as bg color
+        ; at (5,1) as bg color
+        ; at (8,1) as bg color
+    DAT 0x00ef
+    ; Used...
+        ; at (0,0) as fg color
+        ; at (0,1) as fg color
+        ; at (1,0) as fg color
+        ; at (1,1) as fg color
+        ; at (2,0) as fg color
+        ; at (2,1) as fg color
+        ; at (3,0) as fg color
+        ; at (3,1) as fg color
+        ; at (4,0) as fg color
+        ; at (4,1) as fg color
+        ; at (5,0) as fg color
+        ; at (5,1) as fg color
+        ; at (6,0) as fg color
+        ; at (6,1) as fg color
+        ; at (7,0) as fg color
+        ; at (7,1) as fg color
+        ; at (8,0) as fg color
+        ; at (8,1) as fg color
+    DAT 0x0000
+    ; Used...
+        ; at (0,1) as bg color
+        ; at (1,0) as bg color
+        ; at (1,1) as bg color
+        ; at (3,1) as bg color
+        ; at (4,0) as bg color
+        ; at (4,1) as bg color
+        ; at (6,1) as bg color
+        ; at (7,0) as bg color
+        ; at (7,1) as bg color
+    DAT 0x01ff
